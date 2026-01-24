dc7d384253c55d77f94c74e443b32c2266223949
IDGS-804-23002168 <82916@alumnos.utleon.edu.mx>
2026-01-23 19:15:09 -0600

    distancia

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import math
 
 app = Flask(__name__)
 app.secret_key = 'clave_secreta'
@@ -14,12 +15,42 @@ def saludo1():
     return render_template('saludo1.html')
 
 @app.route("/saludo2")
-def saludo2():
+def saludo22():
     return render_template('saludo2.html')
 
-@app.route("/OperasBas")
+@app.route("/distancia", methods=['GET', 'POST'])
+def saludo2():
+    distancia = None
+
+    if request.method == 'POST':
+        x1 = float(request.form['x1'])
+        y1 = float(request.form['y1'])
+        x2 = float(request.form['x2'])
+        y2 = float(request.form['y2'])
+
+        distancia = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+    return render_template('distancia.html', distancia=distancia)
+
+@app.route("/operasBas", methods=['GET','POST'])
 def operasbas():
-    return render_template("operasBas.html")
+    res=None
+    if request.method == 'POST':
+        n1=request.form.get('num1')
+        n2=request.form.get('num2')
+        if request.form.get('operacion') == 'suma':
+            res=float(n1)+float(n2)
+            
+        if request.form.get('operacion') == 'restar':
+            res=float(n1)-float(n2)
+            
+        if request.form.get('operacion') == 'dividir':
+            res=float(n1)/float(n2)
+            
+        if request.form.get('operacion') == 'multiplicar':
+            res=float(n1)*float(n2)
+            
+    return render_template("operasBas.html", res=res)
 
 @app.route("/resultado", methods=['GET', 'POST'])
 def result1():
@@ -65,5 +96,5 @@ def operas():
 '''
 
 if __name__ == '__main__':
-        app.run(host="127.0.0.1", port=5050,debug=True)
+        app.run(host="127.0.0.1", port=5051,debug=True)
     
